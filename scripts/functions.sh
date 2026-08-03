f8359d1f090d42d8c834f66490ee362a878f5f91
Leopoldo <polo.de.esparta@outlook.com>
2026-08-02 21:47:33 -0600

    add validation function

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -2,12 +2,30 @@
 
 
 validate_model(){
+    echo "Validating Model model before start"
+    
     local api_key=$1
     local model=$2
 
-    curl --silent --show-error \
+    local response_file=$(mktemp)
+    local http_code=$( curl --silent --show-error \
+        --output "$response_file" \
+        --write-out "%{http_code}" \
         "https://api.openai.com/v1/models/${model}" \
-        --header "Authorization: Bearer ${api_key}" \
-  
+        --header "Authorization: Bearer ${api_key}"  )
+
+    local response_body=$(cat $response_file)
+
+    rm -f "$response_file"
+
+    if [ "${http_code}" -ne 200  ]; then
+        echo "❌ Error, API call failed with HTTP status code ${http_code}"
+        echo "Response: ${response_body}"
+        echo "Verify your OPENAI_API_KEY is still valid or the model you try to use"
+        return 1
+    fi
+
+    echo "✅ Success: Model '${model}' is valid and ready."
+    return 0  
 
 }
--- start_chat.bash
+++ start_chat.bash
@@ -22,29 +22,13 @@ if [ -n "${ERROR_FOUND}" ]; then
 fi
 
 
-echo "Validating Model model before start"
-
-response_file=$(mktemp)
-
-http_code=$( curl --silent --show-error \
-    --output "$response_file" \
-    --write-out "%{http_code}" \
-    "https://api.openai.com/v1/models/${OPENAI_MODEL}" \
-    --header "Authorization: Bearer ${OPENAI_API_KEY}"  )
-
-response_body=$(cat $response_file)
-
-rm -f "$response_file"
-
-echo "Status Code: ${http_code}"
-echo "Response: ${response_body}"
-
-if [ "${http_code}" -ne 200  ]; then
-    echo "Error, API call failed with HTTP status code ${http_code}"
-    echo "Verify your OPENAI_API_KEY is still valid or the model you try to use"
+if validate_model "${OPENAI_API_KEY}" "${OPENAI_MODEL}"; then
+    bash ./scripts/execute_api_v1_responses.sh
+else
+    echo "Initialization Failed..."
     exit 1
 fi
 
-bash ./scripts/execute_api_v1_responses.sh 
+ 
 
 echo "######################################## Ending BashGPT ######################################## "
